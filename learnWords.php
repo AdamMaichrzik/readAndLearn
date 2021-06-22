6b741f0c15d80fc2e0cb085f14fac437858b603a
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-06-22 12:03:56 +0200

    php files

---
+++ learnWords.php
@@ -0,0 +1,29 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php
+        /* Connection with sql */
+        require_once "dbconnect.php";
+        $connection = mysqli_connect($host, $user, $password) or die ("Error " . mysqli_error());
+        $db = mysqli_select_db($connection, "serwer69488_Playground") or die ("Error " . mysqli_error());
+        $sql = mysqli_query($connection, "SELECT * FROM words");
+
+        /* Loop for taking the data from sql and putting it into php arrays */
+        while($row = mysqli_fetch_array($sql))
+        {
+            $EnglishWords[] = $row["EnglishWords"];
+            $EnglishSentence[] = $row["EnglishSentence"];
+            $PolishWords[] = $row["PolishWords"];
+            $PolishSentence[] = $row["PolishSentence"];
+        }
+  
+    ?>
+
+</head>
+<body>
+    
+</body>
+</html>
