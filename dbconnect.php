f1ab1fe0cd698fdde2f891823bd672fa09639e0b
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-06-22 11:53:04 +0200

    Connection to mysql

---
+++ dbconnect.php
@@ -0,0 +1,6 @@
+<?php
+	$host="sql51.lh.pl"; // Host name
+	$user="serwer69488_Playground"; // mySQL host name 
+	$password="Adam2308"; // Pass for database
+	$database="serwer69488_Playground"; // Name of database
+?>
