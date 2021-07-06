e582eb391c985d88063f668ba0257c06dfebfc06
Adam Maichrzik <adam.maichrzik@wp.pl>
2021-07-06 18:17:35 +0200

    Sending remember word points to all of the words

--- englishLanguage/sendEnglishActiveWord.php
+++ englishLanguage/sendEnglishActiveWord.php
@@ -13,3 +13,40 @@
         echo '<script>window.location = "englishLanguageMainPage.html";</script>';
     }   
 ?>
+
+<?php        
+    if(isset($_POST['rememberWord']))
+    {
+        require_once "../dbconnect.php";
+        //connect to mysqli database (Host/Username/Password)
+        $connection = mysqli_connect($host, $user, $password) or die("Error " . mysqli_error());
+        //select MySQLi dabatase table
+        $db = mysqli_select_db($connection, "serwer69488_Playground") or die("Error " . mysqli_error());  
+        $sql = mysqli_query($connection, "SELECT * FROM englishWordsTable");
+        //Sending data to DB
+    } 
+
+    $phpID = $_COOKIE["englishWordRememberedID"];
+    $phpEnglishWordRemember = $_COOKIE['isEnglishWordRememberedCookie'];
+    $phpTableLength = $_COOKIE['columnLengthCookie'];
+    //echo $phpTableLength;
+    $i = 0;
+
+    $arrsingleresult = str_replace(',', '', $phpEnglishWordRemember);
+   
+    echo $phpEnglishWordRemember;echo "<br>";
+    echo $phpTableLength; echo "<br>";
+    echo $arrsingleresult; echo "<br>";
+    echo $arrsingleresult[4]; echo "<br>";
+    echo $i; echo "<br>";
+
+    while($i <= $phpTableLength)
+    {
+
+        $sql = $connection -> 
+        query("UPDATE `englishWordsTable` SET `isEnglishWordRemembered`= ".$arrsingleresult[$i]." WHERE wordID = ".$i."");
+
+        $i++;
+    }
+    
+?>
